3403cc13876e2fa27fe0c53dc715eb9621bae5e9
ryota.ito <itomise.mizumi@icloud.com>
2021-03-13 14:26:54 +0900

    fallback

--- src/pages/blogs/[id].tsx
+++ src/pages/blogs/[id].tsx
@@ -42,7 +42,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     params: { id: item.id.toString() }
   }))
 
-  return { paths, fallback: true }
+  return { paths, fallback: false }
 }
 
 export const getStaticProps: GetStaticProps = async ({
